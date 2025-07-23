5e450473caa185fb30716b05408d8d47d700e2ee
Keerthiswaran K R <keerthiswarankrk@gmail.com>
2025-07-23 22:08:22 +0530

    yaml update

---
+++ dist/wwwroot/auth.js
@@ -0,0 +1,22 @@
+﻿namespace AuthService.wwwroot
+{
+    public class auth
+    {
+        window.googleSignIn = async () => {
+            const { data, error } = await window.supabase.auth.signInWithOAuth({
+                provider: 'google'
+            });
+
+            if (error) {
+                console.error("OAuth error:", error.message);
+            }
+        };
+
+window.getSupabaseUser = async () => {
+            const { data, error } = await window.supabase.auth.getUser();
+            if (error) return null;
+            return data.user;
+        };
+
+    }
+}
